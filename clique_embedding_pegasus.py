c4da9e3c72a049c13e2a182ca687001b7bc9db13
Joel M. Gottlieb <jgottlieb@dwavesys.com>
2020-08-10 11:05:41 -0400

    Drawing embeddings

---
+++ clique_embedding_pegasus.py
@@ -0,0 +1,64 @@
+# Copyright 2020 D-Wave Systems Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import dimod
+from minorminer import find_embedding
+from dwave.system.samplers import DWaveSampler
+from pyqubo import Array
+import networkx as nx
+import dwave_networkx as dnx
+import matplotlib
+from matplotlib import pyplot as plt
+import sys
+
+
+# Graph partitioning as full mesh
+gamma = 3
+N = int(sys.argv[1])
+
+# Set up variables
+x = Array.create('x', shape=N, vartype='BINARY')
+
+H = gamma * (sum(x) - (N/2)) ** 2
+for i in range(N):
+    for j in range(i + 1, N):
+        H += (x[i] - x[j]) ** 2
+
+chainstrength = 5
+numruns = 100
+
+# Compile the model, and turn it into a QUBO object
+model = H.compile()
+Q, offset = model.to_qubo()
+bqm = dimod.BinaryQuadraticModel.from_qubo(Q, offset=offset)
+
+# Need to relabel variables for the first figure
+bqm2 = bqm.relabel_variables({curr: v for v, curr in enumerate(bqm.variables)}, inplace=False)
+
+# Do the embedding
+dwave_sampler = DWaveSampler(solver={'topology__type__eq': 'pegasus'})
+A = dwave_sampler.edgelist
+embedding = find_embedding(Q, A)
+
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 6))
+
+# Draw the QUBO as a networkx graph
+G = bqm2.to_networkx_graph()
+pos = nx.spring_layout(G)
+nx.draw_networkx(G, pos=pos, font_size=10, node_size=150, ax=axes[0])
+
+# Draw the embedded graph
+G = dnx.pegasus_graph(16)
+dnx.draw_pegasus_embedding(G, embedding, embedded_graph=bqm.to_networkx_graph(), unused_color=None, ax=axes[1])
+plt.show()
