2b250590452c1f5e3b298259c5b214c97f89fadc
Victoria Goliber <vgoliber@dwavesys.com>
2021-03-30 14:02:31 +0200

    Update var colors to match chain colors

--- clique_embedding_pegasus.py
+++ clique_embedding_pegasus.py
@@ -12,25 +12,36 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import sys
+from random import random
+
 import matplotlib.pyplot as plt
 import networkx as nx
 import dwave_networkx as dnx
 from minorminer.busclique import find_clique_embedding
 from dwave.system.samplers import DWaveSampler
 
+try:
+    import matplotlib.pyplot as plt
+except ImportError:
+    matplotlib.use("agg")
+    import matplotlib.pyplot as plt
+
 N = int(sys.argv[1])
 G = nx.complete_graph(N)
 
 fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 6))
 
+node_color_list = [(random(), random(), random()) for i in range(N)]
+chain_color_list = {i: node_color_list[i] for i in range(N)}
+
 # Draw the QUBO as a networkx graph
 pos = nx.spring_layout(G)
-nx.draw_networkx(G, pos=pos, font_size=10, node_size=100, node_color='cyan', ax=axes[0])
+nx.draw_networkx(G, pos=pos, font_size=10, node_size=300, node_color=node_color_list, edge_color='gray', ax=axes[0])
 
 # Draw the embedded graph
 dwave_sampler = DWaveSampler(solver={'topology__type__eq': 'pegasus'})
 A = dwave_sampler.edgelist
 pegasus_graph = dnx.pegasus_graph(16, edge_list=A)
 clique_embedding = find_clique_embedding(N, pegasus_graph)
-dnx.draw_pegasus_embedding(pegasus_graph, clique_embedding, embedded_graph=G, unused_color=None, ax=axes[1])
+dnx.draw_pegasus_embedding(pegasus_graph, clique_embedding, embedded_graph=G, chain_color=chain_color_list, unused_color=None, ax=axes[1])
 plt.savefig('clique_embedding_pegasus')
